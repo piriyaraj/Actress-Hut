f691817063644ba27576ee2a9a8697c964e19e78
piriyaraj <piriyaraj1998@gmail.com>
2023-12-09 13:57:56 +0530

    update

--- main.py
+++ main.py
@@ -7,6 +7,39 @@ import shutil
 from discordwebhook import Discord
 
 import utils.videoUploader as videoUploader
+
+import cv2
+
+def get_video_info(video_path):
+    # Open the video file
+    cap = cv2.VideoCapture(video_path)
+
+    # Check if the video file is opened successfully
+    if not cap.isOpened():
+        print("Error: Could not open the video file.")
+        return
+
+    # Get the width and height of the video
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    # Get the frames per second (FPS) of the video
+    fps = cap.get(cv2.CAP_PROP_FPS)
+
+    # Get the total number of frames in the video
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    # Calculate the duration of the video in seconds
+    duration_seconds = frame_count / fps
+
+    # Calculate the aspect ratio
+    aspect_ratio = width / height
+
+    # Release the video capture object
+    cap.release()
+
+    return width, height, fps, duration_seconds, aspect_ratio
+
 if not os.path.exists('media/videos'):
     os.makedirs('media/videos')
 
@@ -88,6 +121,8 @@ except:
     pass
 finally:
     time.sleep(5)
+exit()
+
 folderPath = [file for file in os.listdir() if file.startswith("post_")]
 if len(folderPath) == 0:
     print("No new folder path found")
--- test.py
+++ test.py
@@ -1,15 +1,8 @@
-import os
-folderPath = [file for file in os.listdir() if file.startswith("post_")]
 
-videoPath = [f for f in os.listdir(folderPath[0]) if f.endswith('.mp4')]
-if len(videoPath) == 0:
-    exit()
-videoList = [f for f in os.listdir(folderPath[0]) if f.endswith('.mp4')]
-if len(videoList) == 0:
-    exit()
-
-videoPath = os.path.abspath(os.path.join(folderPath[0],videoList[0]))
-profile_name_filepath = os.path.join(folderPath[0], 'profile_name.txt')
-with open(profile_name_filepath, 'r', encoding='utf8') as file:
-    profile_name = file.readline().strip()
-print(videoPath.split("post_")[1].split("_")[0])
+# Example usage
+video_path = 'path/to/your/video/file.mp4'
+width, height, fps, duration, aspect_ratio = get_video_info(video_path)
+print(f"Video Dimensions: {width} x {height}")
+print(f"Frames Per Second (FPS): {fps}")
+print(f"Video Duration: {duration:.2f} seconds")
+print(f"Aspect Ratio: {aspect_ratio:.2f}")
